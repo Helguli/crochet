75c8285e6292afbc945cbb1048c4ff065e223fea
Helga ALbert-Huszár <helguli@yahoo.com>
2024-11-20 13:14:48 +0100

    Printing rows with increases

--- ideal_sphere.py
+++ ideal_sphere.py
@@ -1,21 +1,37 @@
 #! /bin/python
 
-import sys
 import numpy as np
 from matplotlib import pyplot as plt
 
-if (len(sys.argv) < 2):
-    print("Please specify a number as command line argument. This number will be the row number of the sphere.")
-    exit()
-
-n = int(sys.argv[1])
+n = int(input("How many rows do you want your sphere to have?\n"))
 angle = np.pi / (n + 1)
 
 x = np.arange(0, np.pi + 0.01, 0.02)
 y = np.sin(x) * n * 2
 
 x2 = np.arange(0, np.pi + 0.01, angle)
-y2 = np.around(np.sin(x2) * n * 2, 0)
+y2 = np.around(np.sin(x2) * n * 2, 0).astype(int)
+
+for i, num in enumerate(y2):
+    if (i == 0):
+        continue
+    if (i == 1):
+        print("Row 1: sc", num, "into magic ring")
+        continue
+    prev = y2[i - 1]
+    if (prev < num):
+        # increase
+        d = num - prev
+        avail = prev - d
+        if (d == 1):
+            print("Row", i, ': sc', int(avail / 3), ', inc, sc', avail - int(avail / 3))
+        else:
+            k = int(avail / d)
+            r = avail % d
+            print('Row', str(i) + ': repeat', d, 'times [sc', k, ', inc]', '' if r > 0 else '\n', end='')
+            if r > 0:
+                print('sc', r)
+
 
 print(y2)
 
